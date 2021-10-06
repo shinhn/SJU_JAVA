8e9105aca4836ff643bc9089c9d4335c15f08f92
KangShinHyeon <rkdrpgml@naver.com>
2021-10-06 16:33:37 +0900

    입력 및 출력 예제 추가

---
+++ 1_print_scanner/src/Ex02_01.java
@@ -0,0 +1,26 @@
+
+public class Ex02_01 {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		int result;
+		
+		a = 100;
+		b = 50;
+		
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a - b;
+		System.out.println(a + "-" + b + "=" + result);
+		
+		result = a * b;
+		System.out.println(a + "*" + b + "=" + result);
+		
+		result = a / b;
+		System.out.println(a + "/" + b + "=" + result);
+		
+	}
+
+}
---
+++ 1_print_scanner/src/Ex02_02.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex02_02 {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		int result;
+		
+		Scanner s = new Scanner(System.in);
+		
+		a = s.nextInt();
+		b = s.nextInt();
+		
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a - b;
+		System.out.println(a + "-" + b + "=" + result);
+		
+		result = a * b;
+		System.out.println(a + "*" + b + "=" + result);
+		
+		result = a / b;
+		System.out.println(a + "/" + b + "=" + result);
+		
+	}
+
+}
